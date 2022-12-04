bd5266c45fbd7dd85654172600706ceb63c72aa1
brahianr <brahian-restrepo@hotmail.com>
2022-12-03 22:57:59 -0500

    Se realizan los ejercicios

--- src/test/java/com/serenitydojo/datatypes/WhenWorkingWithWholeNumbers.java
+++ src/test/java/com/serenitydojo/datatypes/WhenWorkingWithWholeNumbers.java
@@ -15,6 +15,8 @@ public class WhenWorkingWithWholeNumbers {
         // TODO: create a new int variable called timeJump and assign it a value
         //  Next, add this variable to initialYear and assign the result to targetYear, so that targetYear is equal to 2015
 
+        int timeJUmp=30;
+        targetYear=initialYear+timeJUmp;
         assertThat(targetYear, equalTo(2015));
     }
 }
